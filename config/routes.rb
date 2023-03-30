2a0a300e586bd3d4e7c5ac12e6b24703cbc5fe17
Krishna <libraryismyparadise@gmail.com>
2023-03-30 23:38:08 +0545

    Add get and post Routes explicitely

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-    namespace :api do
-    resources :reservations, only: [:index, :create, :destroy]
+  resources :items do
+    resources :reservations, only: [:create]
   end
+  get 'reservations', to: 'reservations#show'
+  post '/reservations', to: 'reservations#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
