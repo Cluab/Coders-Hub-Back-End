dd31a1221ceb1cc69c8e064084354ce56e302f47
cluab <cluab11222@gmail.com>
2023-04-01 20:35:26 +0200

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, format: URI::MailTo::EMAIL_REGEXP
   enum role: %i[user admin]
+  def self.authenticate(email, password)
+    user = User.find_for_authentication(email: email)
+    user&.valid_password?(password) ? user : nil
+  end
 end
