4ac9a87cdf50b87e1f67cf0a971a9e6d557b7027
omarbabou <obabou50@gmail.com>
2023-04-04 01:13:50 +0200

    Add delete functionality to the project

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -13,6 +13,12 @@ class Api::V1::ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    render json: { message: 'Item deleted' }, status: :ok
+  end
+
   private
 
   def item_params
