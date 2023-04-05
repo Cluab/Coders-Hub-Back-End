7ca2110c3706921da88ce56a81fb4b5871bd9dfc
Krishna <libraryismyparadise@gmail.com>
2023-04-05 16:02:41 +0545

    Fix Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
 
-  resources :items do
-    resources :reservations, only: [:create]
-  end
-  get 'reservations', to: 'reservations#show'
-  post '/reservations', to: 'reservations#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  namespace :api do
-    namespace :v1 do
-      get 'items', to: 'items#index'
-      post 'items', to: 'items#create'
-      get 'items/(:id)', to: 'items#show'
+namespace :api do
+  namespace :v1 do
+    resources :items, only: [:index, :create, :show] do
+      resources :reservations, only: [:create]
     end
+    resources :reservations, only: [:index, :show], controller: 'reservations'
   end
+end
+
 end
