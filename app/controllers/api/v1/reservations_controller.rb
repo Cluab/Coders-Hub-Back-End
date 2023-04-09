16103e6a406ac128e5aabdff145178a1ecd2694a
omarbabou <obabou50@gmail.com>
2023-04-09 15:13:13 +0200

    Add destroy to the reservation

---
+++ app/controllers/api/v1/reservations_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::ReservationsController < ApplicationController
+  before_action :set_item, only: [:create]
+  before_action :set_current_user, only: %i[create show]
+
+  def index
+    @reservation = Reservation.all
+    @reservation = @reservation.sample
+    render json: @reservation
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    @reservation.item = @item
+    @reservation.user = @current_user
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    reservation = Reservation.find(params[:id])
+    reservation.destroy
+    render json: { message: 'Reservation deleted' }, status: :ok
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+    render json: @reservation
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def set_current_user
+    @current_user = User.find(session[:user_id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:date, :city)
+  end
+end
