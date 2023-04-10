f581b8839e8d5a612fdb75cf5182f68bb0f0acb2
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-04-10 16:31:36 +0200

    Delete vendor/bundle/ruby/3.1.0 directory

--- vendor/bundle/ruby/3.1.0/gems/rack-cors-2.0.1/lib/rack/cors/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Rack
-  class Cors
-    VERSION = '2.0.1'
-  end
-end
--- vendor/bundle/ruby/3.1.0/gems/rubocop-ast-1.28.0/lib/rubocop/ast/version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module RuboCop
-  module AST
-    module Version
-      STRING = '1.28.0'
-    end
-  end
-end
