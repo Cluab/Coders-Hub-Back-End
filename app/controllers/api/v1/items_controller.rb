94426b5d52fcd032aed799e428bf56bd5b28c6b9
cluab <cluab11222@gmail.com>
2023-04-10 16:43:24 +0200

    fix backend bad merge

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,34 +7,36 @@ module Api
         render json: @items
       end
 
-  def show
-    @item = Item.find(params[:id])
-    render json: @item
-  end
+      def show
+        @item = Item.find(params[:id])
+        render json: @item
+      end
 
-  def create
-    item = Item.new(item_params)
-    if item.save
-      render json: item, status: :created
-    else
-      render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
+      def create
+        item = Item.new(item_params)
+        if item.save
+          render json: item, status: :created
+        else
+          render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
 
-  # DELETE /items/1
-  def destroy
-    @item = Item.find(params[:id])
-    # refressh id
-    if @item.destroy
-      render json: { message: 'Item deleted' }
-    else
-      render json: @reservation.errors, status: :unprocessable_entity
-    end
-  end
+      # DELETE /items/1
+      def destroy
+        @item = Item.find(params[:id])
+        # refressh id
+        if @item.destroy
+          render json: { message: 'Item deleted' }
+        else
+          render json: @reservation.errors, status: :unprocessable_entity
+        end
+      end
 
       private
 
-  def item_params
-    params.require(:item).permit(:name, :description, :photo, :price, :mentor_name, :duration, :user_id)
+      def item_params
+        params.require(:item).permit(:name, :description, :photo, :price, :mentor_name, :duration, :user_id)
+      end
+    end
   end
 end
