f589b7ab442f81588a726fd7aff72ad5e24ac969
omarbabou <obabou50@gmail.com>
2023-04-10 10:02:05 +0200

    Update some files

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :items, only: %i[index show create destroy]
       get 'items', to: 'items#index'
       post 'items', to: 'items#create'
-      post 'items', to: 'items#destroy'
       get 'items/(:id)', to: 'items#show'
     end
   end
