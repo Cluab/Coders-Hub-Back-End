c4dd2ea45c7f3b5813eb2cc43ba2993636ee7f2f
Mancoba Sihlongonyane <manq2010@gmail.com>
2023-04-10 15:38:47 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,6 @@ Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   use_doorkeeper
   devise_for :users
-
-  namespace :api do
-    namespace :v1 do
-      resources :items, only: %i[index show create destroy]
-      get 'items', to: 'items#index'
-      post 'items', to: 'items#create'
-      get 'items/(:id)', to: 'items#show'
-    end
-  end
   
   draw :api
 end
