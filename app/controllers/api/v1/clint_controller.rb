6d5c0a46c01921a997ce7d2ed3345358be6c9635
cluab <cluab11222@gmail.com>
2023-04-11 07:50:04 +0200

    Add React-Clint Controller

---
+++ app/controllers/api/v1/clint_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class UsersController < ApiController
+      skip_before_action :doorkeeper_authorize!, only: %i[create]
+
+      # GET /clint.json
+      def clint
+        @react_clint = Doorkeeper::Application.find_by(name: 'React')
+        if @react_clint.nil?
+          render json: { error: 'no React Clint Present' }, status: :unprocessable_entity
+        else
+          render json: {
+            id: @react_clint.id,
+            name: @react_clint.name,
+            uid: @react_clint.uid,
+            secret: @react_clint.secret,
+            redirect_uri: @react_clint.redirect_uri,
+            scopes: @react_clint.scopes,
+            confidential: @react_clint.confidential,
+            created_at: @current_user.created_at.iso8601
+          }, status: :ok
+        end
+      end
+    end
+  end
+end
